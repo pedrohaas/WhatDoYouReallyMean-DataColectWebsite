b91f344ba76c8d32c8bba9514c4520d8578c9d56
pedrohaas <pedrohaa.silva@gmail.com>
2025-10-26 20:05:07 +0100

    fix: remover import uuidv4 e incluir função uuidv4 nativa no script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+function uuidv4() {
+  if (crypto && crypto.randomUUID) {
+    return crypto.randomUUID();
+  }
+  // Fallback manual se o navegador não tiver suporte:
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
+
 let frases = [];
 let indice = 0;
 let userId = uuidv4();
